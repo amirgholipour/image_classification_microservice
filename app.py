2fb425d9da276f08cf22391c7295c43fa3c7f110
saeed-kasmani <saeed.kasmani@ibm.com>
2023-04-26 00:17:28 +1000

    update with new html webpage

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-# app.py
 import os
 from flask import Flask, request, jsonify
 import numpy as np
 from inference import infer
 from model import load_image, preprocess_image
-
+from flask_cors import CORS
+from heapq import nlargest
 app = Flask(__name__)
-
+CORS(app)
 with open("ImageNetLabels.txt", "r") as f:
-    class_labels = f.read().splitlines()[1:]  # Skip the first line, which is 'background'
+    class_labels = f.read().splitlines()[1:]
 
 @app.route('/classify', methods=['POST'])
 def classify():
@@ -23,23 +23,33 @@ def classify():
     probabilities = infer(image)
     predicted_class_index = np.argmax(probabilities)
     predicted_class_name = class_labels[predicted_class_index]
-    # Print the list of existing files
-    file_list = os.listdir('/tmp')
-    print('List of existing files:')
-    for f in file_list:
-        print(f)
+
+    #top_k = 5
+    #top_indices = nlargest(top_k, range(len(probabilities)), probabilities.take)
+    #top_probabilities = probabilities[top_indices]
+    #top_5_predictions = [class_labels[i] for i in top_indices]
+    #print(top_indices, top_probabilities, top_5_predictions)
+
+    #return jsonify({"top_classes": top_class_names, "top_probabilities": top_probabilities.tolist()})
 
 
+    #top_5 = 5  # Change this to display a different number of predictions
+    #top_5_indices = np.argpartition(probabilities, -top_5)[-top_5:]
+    #top_5_predictions = [(class_labels[index], float(probabilities[index])) for index in top_5_indices]
+
+    probabilities  = np.squeeze(probabilities)
+    top_5_indices = np.argsort(probabilities)[-5:]#[::-1]
+    print(top_5_indices)
+    probabilities = np.array(probabilities, dtype=np.float32)  # Add this line
+    top_5_predictions = [(class_labels[index], float(probabilities[index])) for index in top_5_indices]
+
+    return jsonify({"top_5_predictions": top_5_predictions})
 
 
-    return jsonify({"predicted_class_name": predicted_class_name})
-
-
-    # Return the response as JSON
-    # return jsonify(probabilities.tolist())
 @app.route('/list_files')
 def list_files():
     files = os.listdir('.')
     return {'files': files}
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
