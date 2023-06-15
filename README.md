170d4cc13303f582a6a2fd2fcccc7a048ca9c66d
saeed-kasmani <saeed.kasmani@ibm.com>
2023-06-16 09:52:54 +1000

    update the readme and app.py

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# image_classification_microservice
+# AI Developer Dockerized Application
+
+This repository contains a Dockerfile and Python code for running an AI-based application that performs image classification and segmentation. The application is built using Flask, a lightweight web framework in Python.
+
+## Prerequisites
+- Docker installed on your machine
+
+## Usage
+
+1. Clone the repository:
+
+   ```
+   git clone <repository_url>
+   ```
+
+2. Build the Docker image:
+
+   ```
+   docker build -t ai-application .
+   ```
+
+3. Run the Docker container:
+
+   ```
+   docker run -p 5001:5000 -d ai-application
+   ```
+
+4. Access the application in your web browser by navigating to `http://localhost:5001`.
+
+## Endpoints
+
+The application provides the following endpoints:
+
+- `POST /classify_and_segment`: This endpoint accepts an image file or URL as input and performs image classification and segmentation. It returns the top 5 predicted classes along with the segmented image encoded in base64 format.
+
+- `GET /list_files`: This endpoint lists all the files in the current directory.
+
+## Additional Files
+
+- `ImageNetLabels.txt`: This file contains the labels for the ImageNet dataset used for image classification.
+
+- `model.py`: This file contains code for loading the image, preprocessing it, and segmenting the image.
+
+- `inference.py`: This file contains the code for performing inference on the loaded image.
+
+- `data/`: This directory contains additional data files required by the application.
+
+## Important Notes
+
+- The application uses the `opencv-python-headless` library with version `4.5.3.56` for image processing. The specific version is installed to ensure compatibility.
+
+- The application runs on port `5001` within the Docker container and is mapped to the same port on the host machine.
+
+- The application assumes that the image classification model and other required files are present in the current directory during the Docker build process.
+
+- Ensure that the necessary Python packages mentioned in `requirements.txt` are available for installation before running the Docker build command.
+
+- The application is set to accept image files or URLs in the `image` field of a multipart/form-data POST request or in the `image_url` field of a JSON POST request.
+
+- The segmented image is returned in base64 format for ease of transmission in JSON.
+
+Feel free to modify the `app.py` file and other relevant files according to your requirements.
+
+Please note that this README assumes familiarity with Docker and basic web development concepts. If you encounter any issues or have questions, feel free to reach out for assistance.
--- app.py
+++ app.py
@@ -1,62 +1,15 @@
 import os
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
 import base64
 import numpy as np
 from inference import infer
-from model import load_image, preprocess_image, segment_image
+from model import load_image, segment_image
 from flask_cors import CORS
-from heapq import nlargest
 app = Flask(__name__)
 CORS(app)
 with open("ImageNetLabels.txt", "r") as f:
     class_labels = f.read().splitlines()[1:]
 
-@app.route('/classify', methods=['POST'])
-def classify():
-    if 'image' in request.files:
-        file = request.files['image']
-    elif 'image_url' in request.json:
-        file = request.json['image_url']
-    else:
-        return jsonify({"error": "No image file or URL provided"}), 400
-
-    image, _ = load_image(file)
-    probabilities = infer(image)
-    predicted_class_index = np.argmax(probabilities)
-    predicted_class_name = class_labels[predicted_class_index]
-
-    #top_k = 5
-    #top_indices = nlargest(top_k, range(len(probabilities)), probabilities.take)
-    #top_probabilities = probabilities[top_indices]
-    #top_5_predictions = [class_labels[i] for i in top_indices]
-    #print(top_indices, top_probabilities, top_5_predictions)
-
-    #return jsonify({"top_classes": top_class_names, "top_probabilities": top_probabilities.tolist()})
-
-
-    #top_5 = 5  # Change this to display a different number of predictions
-    #top_5_indices = np.argpartition(probabilities, -top_5)[-top_5:]
-    #top_5_predictions = [(class_labels[index], float(probabilities[index])) for index in top_5_indices]
-
-    probabilities  = np.squeeze(probabilities)
-    top_5_indices = np.argsort(probabilities)[-5:]#[::-1]
-    print(top_5_indices)
-    probabilities = np.array(probabilities, dtype=np.float32)  # Add this line
-    top_5_predictions = [(class_labels[index], float(probabilities[index])) for index in top_5_indices]
-
-    return jsonify({"top_5_predictions": top_5_predictions})
-@app.route("/segment", methods=["POST"])
-def segment():
-    if 'image' in request.files:
-        file = request.files['image']
-    elif 'image_url' in request.json:
-        file = request.json['image_url']
-    else:
-        return jsonify({"error": "No image file or URL provided"}), 400
-
-    image, _ = load_image(file)
-    buffer = segment_image(image)
-    return send_file(buffer, mimetype="image/png")
 
 
 @app.route("/classify_and_segment", methods=["POST"])
@@ -71,8 +24,6 @@ def classify_and_segment():
     image, _ = load_image(file)
 
     probabilities = infer(image)
-    predicted_class_index = np.argmax(probabilities)
-    predicted_class_name = class_labels[predicted_class_index]
     probabilities  = np.squeeze(probabilities)
     top_5_indices = np.argsort(probabilities)[-5:]#[::-1]
     print(top_5_indices)
